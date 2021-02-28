7e9ba8416ec1f08b11074cfffbe4fb9573d2a268
Christopher Briggs <christophertbriggs@gmail.com>
2021-02-28 05:13:52 -0500

    Work for Assignment 3

---
+++ assignment3/calculator.php
@@ -0,0 +1,31 @@
+<?php
+class Calculator{
+
+    public function calc($operator = NULL, $num1 = NULL, $num2 = NULL)
+    {
+        if(!is_string($operator) || !is_int($num1) || !is_int($num2))    //checks to make sure all the varibles are the correct type
+        {
+            return "You must enter a string and two numbers.<br>";
+        }
+
+        switch($operator)
+        {
+            case "+":
+                return $num1.$operator.$num2." = ".($num1 + $num2)."<br>";
+            case "-":
+                return $num1.$operator.$num2." = ".($num1 - $num2)."<br>";
+            case "*":
+                return $num1.$operator.$num2." = ".($num1 * $num2)."<br>";
+            case "/":
+                if($num2 == 0)
+                {
+                    return "Cannot divide by zero <br>";
+                }
+                return $num1.$operator.$num2." = ".($num1 / $num2)."<br>";
+            default:
+                return $operator." is not a valid operator.<br>";
+        }
+    }
+}
+
+?>
