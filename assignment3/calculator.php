fb885e70e8bc56c046170790e6b9c96d67b90372
Christopher Briggs <christophertbriggs@gmail.com>
2021-02-28 22:19:02 -0500

    Fixed formating

--- assignment3/calculator.php
+++ assignment3/calculator.php
@@ -5,25 +5,25 @@ class Calculator{
     {
         if(!is_string($operator) || !is_int($num1) || !is_int($num2))    //checks to make sure all the varibles are the correct type
         {
-            return "You must enter a string and two numbers.<br>";
+            return "You must enter a string and two numbers<br>";
         }
 
         switch($operator)
         {
             case "+":
-                return $num1.$operator.$num2." = ".($num1 + $num2)."<br>";
+                return "The sum of the numbers is ".($num1 + $num2)."<br>";
             case "-":
-                return $num1.$operator.$num2." = ".($num1 - $num2)."<br>";
+                return "The difference of the numbers is ".($num1 - $num2)."<br>";
             case "*":
-                return $num1.$operator.$num2." = ".($num1 * $num2)."<br>";
+                return "The product of the numbers is ".($num1 * $num2)."<br>";
             case "/":
                 if($num2 == 0)
                 {
                     return "Cannot divide by zero <br>";
                 }
-                return $num1.$operator.$num2." = ".($num1 / $num2)."<br>";
+                return "The division of the numbers is ".($num1 / $num2)."<br>";
             default:
-                return $operator." is not a valid operator.<br>";
+                return $operator." is not a valid operator<br>";
         }
     }
 }
